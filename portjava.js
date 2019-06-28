4e5d8b2d1a42bbfc938af1cd224256627f9c19bb
armjim14 <ajimenez1497@gmail.com>
2019-06-28 15:52:35 -0700

    Float issue fix

---
+++ portjava.js
@@ -0,0 +1,15 @@
+var contact = document.getElementById("contact");
+var portfolio = document.getElementById("portfolio");
+var home = document.getElementById("home");
+
+contact.addEventListener("click", function(){
+    window.location.href = 'contact.html'
+})
+
+portfolio.addEventListener("click", function(){
+    window.location.href = 'portfolio.html'
+})
+
+home.addEventListener("click", function(){
+    window.location.href = 'index.html'
+})
