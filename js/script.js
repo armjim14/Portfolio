32079b0f6a7e0af9c140a382473a5352b3d1d76f
armjim14 <ajimenez1497@gmail.com>
2019-09-05 15:53:06 -0700

    added firebase to email sending

--- js/script.js
+++ js/script.js
@@ -8,143 +8,143 @@
 /* Preloader
 ------------------------------------------------------ */
 
-  $(window).load(function(){
+$(window).load(function () {
 
-    $('.loader').fadeOut();    
-    $('#preloader').delay(350).fadeOut('slow');    
-    $('body').delay(350);   
+   $('.loader').fadeOut();
+   $('#preloader').delay(350).fadeOut('slow');
+   $('body').delay(350);
 
-    });
-
-
- jQuery(document).ready(function($) {
-
-/*----------------------------------------------------*/
-/* Initializing jQuery Nice Scroll
------------------------------------------------------- */
-
-    $("html").niceScroll({
-      cursorcolor:"#11abb0", // Set cursor color
-      cursorwidth: "8", // Sety cursor width
-      cursorborder: "" // Set cursor border color, default left none
-    });
-
-
-/*----------------------------------------------------*/
-/* FitText Settings
------------------------------------------------------- */
-
-    setTimeout(function() {
-	   $('h1.responsive-headline').fitText(1, { minFontSize: '28px', maxFontSize: '72px' });
-	 }, 100);
-
-
-/*----------------------------------------------------*/
-/* Smooth Scrolling
------------------------------------------------------- */
-
-   $('.smoothscroll').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash,
-	    $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
-
-	});
-
-
-/*----------------------------------------------------*/
-/* Appear Animation
-------------------------------------------------------*/
-  new WOW().init();
-
-/*----------------------------------------------------*/
-/* Parallax for Header Content
-------------------------------------------------------*/
-$(window).scroll(function(e){
-  parallax();
 });
 
 
-function parallax() {
-  var scrollPosition = $(window).scrollTop();
-  $('.banner').css('margin-top', (0 - (scrollPosition * .8)) + 'px');
-}
+jQuery(document).ready(function ($) {
 
-/*----------------------------------------------------*/
-/* Highlight the current section in the navigation bar
-------------------------------------------------------*/
+   /*----------------------------------------------------*/
+   /* Initializing jQuery Nice Scroll
+   ------------------------------------------------------ */
 
-	var sections = $("section");
-	var navigation_links = $("#m-nav a");
-
-	sections.waypoint({
-
-      handler: function(event, direction) {
-
-		   var active_section;
-
-			active_section = $(this);
-			if (direction === "up") active_section = active_section.prev();
-
-			var active_link = $('#m-nav a[href="#' + active_section.attr("id") + '"]');
-
-         navigation_links.parent().removeClass("current");
-			active_link.parent().addClass("current");
-
-		},
-		offset: '35%'
-
-	});
-
-
-/*----------------------------------------------------*/
-/*	Make sure that #header-background-image height is
-/* equal to the browser height.
------------------------------------------------------- */
-
-   $('header').css({ 'height': $(window).height() });
-   $(window).on('resize', function() {
-
-        $('header').css({ 'height': $(window).height() });
-        $('body').css({ 'width': $(window).width() })
+   $("html").niceScroll({
+      cursorcolor: "#11abb0", // Set cursor color
+      cursorwidth: "8", // Sety cursor width
+      cursorborder: "" // Set cursor border color, default left none
    });
 
 
-/*----------------------------------------------------*/
-/*  On scroll blur header
-------------------------------------------------------*/
-   (function() {
-      $(window).scroll(function() {
-        var oVal;
-        oVal = $(window).scrollTop() / 100;
-        return $(".header-overlay").css("opacity", oVal);
-        });
+   /*----------------------------------------------------*/
+   /* FitText Settings
+   ------------------------------------------------------ */
 
-      }).call(this);
+   setTimeout(function () {
+      $('h1.responsive-headline').fitText(1, { minFontSize: '28px', maxFontSize: '72px' });
+   }, 100);
 
 
+   /*----------------------------------------------------*/
+   /* Smooth Scrolling
+   ------------------------------------------------------ */
 
-/*----------------------------------------------------*/
-/*	Fade In/Out Primary Navigation
-------------------------------------------------------*/
+   $('.smoothscroll').on('click', function (e) {
+      e.preventDefault();
 
-   $(window).on('scroll', function() {
+      var target = this.hash,
+         $target = $(target);
 
-		var h = $('header').height();
-		var y = $(window).scrollTop();
+      $('html, body').stop().animate({
+         'scrollTop': $target.offset().top
+      }, 800, 'swing', function () {
+         window.location.hash = target;
+      });
+
+   });
+
+
+   /*----------------------------------------------------*/
+   /* Appear Animation
+   ------------------------------------------------------*/
+   new WOW().init();
+
+   /*----------------------------------------------------*/
+   /* Parallax for Header Content
+   ------------------------------------------------------*/
+   $(window).scroll(function (e) {
+      parallax();
+   });
+
+
+   function parallax() {
+      var scrollPosition = $(window).scrollTop();
+      $('.banner').css('margin-top', (0 - (scrollPosition * .8)) + 'px');
+   }
+
+   /*----------------------------------------------------*/
+   /* Highlight the current section in the navigation bar
+   ------------------------------------------------------*/
+
+   var sections = $("section");
+   var navigation_links = $("#m-nav a");
+
+   sections.waypoint({
+
+      handler: function (event, direction) {
+
+         var active_section;
+
+         active_section = $(this);
+         if (direction === "up") active_section = active_section.prev();
+
+         var active_link = $('#m-nav a[href="#' + active_section.attr("id") + '"]');
+
+         navigation_links.parent().removeClass("current");
+         active_link.parent().addClass("current");
+
+      },
+      offset: '35%'
+
+   });
+
+
+   /*----------------------------------------------------*/
+   /*	Make sure that #header-background-image height is
+   /* equal to the browser height.
+   ------------------------------------------------------ */
+
+   $('header').css({ 'height': $(window).height() });
+   $(window).on('resize', function () {
+
+      $('header').css({ 'height': $(window).height() });
+      $('body').css({ 'width': $(window).width() })
+   });
+
+
+   /*----------------------------------------------------*/
+   /*  On scroll blur header
+   ------------------------------------------------------*/
+   (function () {
+      $(window).scroll(function () {
+         var oVal;
+         oVal = $(window).scrollTop() / 100;
+         return $(".header-overlay").css("opacity", oVal);
+      });
+
+   }).call(this);
+
+
+
+   /*----------------------------------------------------*/
+   /*	Fade In/Out Primary Navigation
+   ------------------------------------------------------*/
+
+   $(window).on('scroll', function () {
+
+      var h = $('header').height();
+      var y = $(window).scrollTop();
       var nav = $('#m-nav');
 
-	   if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
-	      nav.fadeOut('fast');
-	   }
+      if ((y > h * .20) && (y < h) && ($(window).outerWidth() > 768)) {
+         nav.fadeOut('fast');
+      }
       else {
-         if (y < h*.20) {
+         if (y < h * .20) {
             nav.removeClass('opaque').fadeIn('fast');
          }
          else {
@@ -152,73 +152,75 @@ function parallax() {
          }
       }
 
-	});
+   });
 
 
-/*----------------------------------------------------*/
-/*	Modal Popup
-------------------------------------------------------*/
+   /*----------------------------------------------------*/
+   /*	Modal Popup
+   ------------------------------------------------------*/
 
-    $('.item-wrap a').magnificPopup({
+   $('.item-wrap a').magnificPopup({
 
-       type:'inline',
-       fixedContentPos: false,
-       removalDelay: 200,
-       showCloseBtn: false,
-       mainClass: 'mfp-fade'
+      type: 'inline',
+      fixedContentPos: false,
+      removalDelay: 200,
+      showCloseBtn: false,
+      mainClass: 'mfp-fade'
 
-    });
+   });
 
-    $(document).on('click', '.popup-modal-dismiss', function (e) {
-    		e.preventDefault();
-    		$.magnificPopup.close();
-    });
-
-
-/*----------------------------------------------------*/
-/*  Owl Carousel
-/*----------------------------------------------------*/
+   $(document).on('click', '.popup-modal-dismiss', function (e) {
+      e.preventDefault();
+      $.magnificPopup.close();
+   });
 
 
-    $(document).ready(function() {
-     
-    $("#testimonial-slides").owlCarousel({
-     
-    navigation : false, // Show next and prev buttons
-    slideSpeed : 300,
-    paginationSpeed : 400,
-    singleItem:true
-     
-    // "singleItem:true" is a shortcut for:
-    // items : 1,
-    // itemsDesktop : false,
-    // itemsDesktopSmall : false,
-    // itemsTablet: false,
-    // itemsMobile : false
-     
-    });
-     
-    });
+   /*----------------------------------------------------*/
+   /*  Owl Carousel
+   /*----------------------------------------------------*/
 
 
-/*----------------------------------------------------*/
-/*  Google Map
-------------------------------------------------------*/
+   $(document).ready(function () {
 
-    // main directions
-      map = new GMaps({
-        el: '#map', lat: 23.790223, lng: 90.414036, zoom: 13, zoomControl : true, 
-        zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, panControl : false, scrollwheel: false
+      $("#testimonial-slides").owlCarousel({
+
+         navigation: false, // Show next and prev buttons
+         slideSpeed: 300,
+         paginationSpeed: 400,
+         singleItem: true
+
+         // "singleItem:true" is a shortcut for:
+         // items : 1,
+         // itemsDesktop : false,
+         // itemsDesktopSmall : false,
+         // itemsTablet: false,
+         // itemsMobile : false
+
       });
-    // add address markers
-    map.addMarker({ lat: 23.790223, lng: 90.414036, title: 'BD InfoSys',
-      infoWindow: { content: '<p>Building # 2, Plot # 111, Road # 35, Gulshan - 2, Dhaka</p>' } });
 
-/*----------------------------------------------------*/
-/*	contact form
-------------------------------------------------------*/
+   });
 
-   $('form#contactForm button.submit').click(function() {
+
+   /*----------------------------------------------------*/
+   /*  Google Map
+   ------------------------------------------------------*/
+
+   // main directions
+   map = new GMaps({
+      el: '#map', lat: 23.790223, lng: 90.414036, zoom: 13, zoomControl: true,
+      zoomControlOpt: { style: 'SMALL', position: 'TOP_LEFT' }, panControl: false, scrollwheel: false
+   });
+   // add address markers
+   map.addMarker({
+      lat: 23.790223, lng: 90.414036, title: 'BD InfoSys',
+      infoWindow: { content: '<p>Building # 2, Plot # 111, Road # 35, Gulshan - 2, Dhaka</p>' }
+   });
+
+   /*----------------------------------------------------*/
+   /*	contact form
+   ------------------------------------------------------*/
+
+   $('form#contactForm button.submit').click(function () {
 
       $('#image-loader').fadeIn();
 
@@ -228,30 +230,30 @@ function parallax() {
       var contactMessage = $('#contactForm #contactMessage').val();
 
       var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
+         '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
 
       $.ajax({
 
-	      type: "POST",
-	      url: "inc/sendEmail.php",
-	      data: data,
-	      success: function(msg) {
+         type: "POST",
+         url: "inc/sendEmail.php",
+         data: data,
+         success: function (msg) {
 
             // Message was sent
             if (msg == 'OK') {
                $('#image-loader').fadeOut();
                $('#message-warning').hide();
                $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();   
+               $('#message-success').fadeIn();
             }
             // There was an error
             else {
                $('#image-loader').fadeOut();
                $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
+               $('#message-warning').fadeIn();
             }
 
-	      }
+         }
 
       });
       return false;
@@ -266,4 +268,39 @@ $("#forGithub").on("click", () => {
 
 $("#forlinkedin").on("click", () => {
    window.open("https://www.linkedin.com/in/armando-jimenez-291a4b180");
+})
+
+$("#firebaseSend").on("click", () => {
+
+   let name = document.getElementById("contactName");
+   let email = document.getElementById("contactEmail");
+   let subj = document.getElementById("contactSubject");
+   let msg = document.getElementById("contactMessage");
+
+   var firebaseConfig = {
+      apiKey: "AIzaSyAAjTkTaaAYcp4y6U4karifugJsRENeiM4",
+      authDomain: "game1-e50a2.firebaseapp.com",
+      databaseURL: "https://game1-e50a2.firebaseio.com",
+      projectId: "game1-e50a2",
+      storageBucket: "game1-e50a2.appspot.com",
+      messagingSenderId: "400261651315",
+      appId: "1:400261651315:web:64034034e67c5cc5"
+   };
+
+   firebase.initializeApp(firebaseConfig)
+
+   var database = firebase.database();
+
+   database.ref().push({
+      name: name.value,
+      email: email.value,
+      subj: subj.value,
+      msg: msg.value
+   })
+
+   name.value = "";
+   email.value = "";
+   subj.value = "";
+   msg.value = "";
+
 })
